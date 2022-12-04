a219c31bed5d38d4c84366f5014d5bd7a2ecdb78
Harsha Bandi <90712404+ggharsha@users.noreply.github.com>
2022-12-04 15:58:40 -0600

    Remove old background from background.js

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -13,23 +13,6 @@ export default class Background extends MovingObject {
     }
 
     draw(ctx) {
-        // old background
-        // // empty space
-        // ctx.fillStyle = '#a9a9a9'
-        // ctx.fillRect(this.pos[0], 600, 1000, 401); // bottom canvas
-        // ctx.fillRect(this.pos[0], 0, 1000, 139); // top canvas
-
-        // // road
-        // ctx.fillStyle = '#838383';
-        // ctx.fillRect(this.pos[0], 400, 1000, 140);
-
-        // // road lines
-        // ctx.fillStyle = 'yellow';
-        // for (let counter = 0; counter < 11; counter++) {
-        //     ctx.fillRect((100 * counter + this.pos[0]), 460, 50, 10);
-        // }; 
-
-        // new background image
         ctx.drawImage(this.img, this.pos[0], this.pos[1]);
     }
 
